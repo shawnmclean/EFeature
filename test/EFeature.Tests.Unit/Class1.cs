1c01b3b253a5ed68fd58094df20889f71352bcb4
Shawn Mclean <shawnrmclean@gmail.com>
2014-09-19 01:41:12 -0500

    testing xunit

---
+++ test/EFeature.Tests.Unit/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EFeature.Tests.Unit
+{
+    public class Class1
+    {
+        [Fact]
+        public void lol()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
