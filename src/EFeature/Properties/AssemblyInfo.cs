2e0161f27818144e4b826663829f3c2acf30d4e4
Shawn Mclean <shawnrmclean@gmail.com>
2014-09-19 01:20:39 -0500

    fixed build

--- src/EFeature/Properties/AssemblyInfo.cs
+++ src/EFeature/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EFeature")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Feature Toggle Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Xormis")]
 [assembly: AssemblyProduct("EFeature")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
